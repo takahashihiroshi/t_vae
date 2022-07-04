b3e580504e8c9c85cf47b214e3812757a948bee1
MauritsOever <mauritsvandenoever@hotmail.com>
2022-07-04 15:26:35 +0200

    summary

--- test_code.py
+++ test_code.py
@@ -18,7 +18,7 @@ from data.datafuncs import GetData, GenerateAllDataSets
 X_train, weights = GetData('returns')
 
 n_in = X_train.shape[1]
-n_latent = 10
+n_latent = 25
 n_h = round((n_in + n_latent)/2)
 
 dist = 'normal'
@@ -26,6 +26,8 @@ dist = 't'
 
 if dist == 'normal':
     model = GaussianVAE(n_in, n_latent, n_h)
+elif dist == 't':
+    model = StudentsTVAE(n_in, n_latent, n_h)
 
 save_path = os.path.join(os.getcwd(), r'results')
 
@@ -34,10 +36,11 @@ model.fit(X_train, k=1, batch_size=100,
           warm_up=False, is_stoppable=True,
           X_valid=X_train, path=save_path)
 
+if dist == 'normal':
+    reconstruct, _ = model.reconstruct(X_train)
+elif dist == 't':
+    _, reconstruct, _ = model.reconstruct(X_train)
 
-z, _ = model.encode(X_train)
-
-reconstruct, _ = model.decode(z)
 
 print(((X_train - reconstruct)**2).mean())
 
